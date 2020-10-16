b6f8e17eb592debed679a9dfde5b00f7c059e032
Ross Zimmerman <raz2124@columbia.edu>
2020-10-16 22:06:50 +0000

    Update map, started sightings, .gitignore not work

--- squirrels/urls.py
+++ squirrels/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #path('sightings', views.trial, name='trial') # not sure if we need an extra one here
 ]
--- squirrels/views.py
+++ squirrels/views.py
@@ -1,4 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("This is where Maps View will go.")
+    #return HttpResponse('Ths is where Maps will go')
+    return render(request, 'map/index.html', {})
+
+
+#Placeholder for sightings
+#def trials(request):
+#    return HttpResponse("This is where Sightings View will go.")
+
+
+#Placeholder for sightings/<unique-squirrel-id>
+#def index(request):
+#    return HttpResponse("This is where Sightings-unique-id View will go.")
+
+
+
+#Placeholder for sightings/add
+#def index(request):
+#    return HttpResponse("This is where Sightings/add View will go.")
