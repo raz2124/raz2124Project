32ef495e0b62f04d7ca774b623d94c16aa3d5070
Ross Zimmerman <raz2124@columbia.edu>
2020-10-17 01:54:47 +0000

    Initial Leaflet map and Squirrel model initialized

--- squirrels/models.py
+++ squirrels/models.py
@@ -3,18 +3,26 @@ from django.utils.translation import gettext as _
 
 
 class Squirrel(models.Model):
-    
     #at 3:30 in 8.9 he said id should auto generate - Django should do that
-
     name = models.CharField(
            max_length=100,
-           help_text=_('Name of pet)',
+           help_text=_('Name of pet'),
     )
-    
-    date = models.DateField( 
-         help_text=_('Day the Squirrel was Sighted'),
-    )   
+   # sighting_date = models.DateField( 
+   #     help_text=_('Day the Squirrel was Sighted'),
+   # )   
+   
+   # vaccinated = models.BooleanField(
+   #      help_text=_('Is pet vaccinated'),
+   # )
 
-    date = models.DateField(
-         help_text=_('Day the Squirrel was Sighted'),
-    )
+   # profile_image = models.ImageField(
+   #     help_text=_('Profile pic of squirrel'),
+   #     blank=True,
+   #     upload_to='profiles_images',
+   # )
+    
+   # bio = models.TextField(
+   #     blank=True,
+   # )
+
