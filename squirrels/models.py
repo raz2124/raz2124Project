9070ea0e4e4906070efc0539e1cc004f86de66a3
Ross Zimmerman <raz2124@columbia.edu>
2020-10-17 01:11:10 +0000

    minor additions to models

--- squirrels/models.py
+++ squirrels/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Squirrel(models.Model):
+    
+    #at 3:30 in 8.9 he said id should auto generate - Django should do that
+
+    name = models.CharField(
+           max_length=100,
+           help_text=_('Name of pet)',
+    )
+    
+    date = models.DateField( 
+         help_text=_('Day the Squirrel was Sighted'),
+    )   
+
+    date = models.DateField(
+         help_text=_('Day the Squirrel was Sighted'),
+    )
