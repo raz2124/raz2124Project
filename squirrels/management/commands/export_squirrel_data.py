caee9b31f857ec5bb7cde9d10a310b20bc716f51
Emily Evans <ere2119@columbia.edu>
2020-10-20 20:30:14 +0000

    made changes to export command

--- squirrels/management/commands/export_squirrel_data.py
+++ squirrels/management/commands/export_squirrel_data.py
@@ -10,5 +10,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         file_ = options['squirrel_file']
 
+        
+        with open(file_, 'w') as fp:
+            writer = csv.writer(fp)
+            writer.writerow(['X','Y','Unique Squirrel ID','Shift', 'Date','Age'])
+
+            for squirrel in Squirrel.objects.all().value_list('latitude','longitude','unique_id','shift','date','age'):
+                writer.writerow(squirrel)
+        
+        
         msg = f'You are exporting to {file_}'
         self.stdout.write(self.style.SUCCESS(msg))
